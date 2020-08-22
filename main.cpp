26986d6eba3fa08660111bdb7437756b2b4013b2
shahmeerathar <shahmeer.athar@gmail.com>
2020-08-22 16:30:52 -0400

    Expanded write_ppm to expand various resolutions

--- main.cpp
+++ main.cpp
@@ -9,12 +9,17 @@ void write_ppm()
     {
         int width = 256;
         int height = 256;
+        int remaining = width * height;
         image << "P3\n" << width << ' ' << height << "\n255\n";
-        for (int i = 0; i <= 255; i++)
+        for (int i = 0; i < height; i++)
         {
-            for (int j = 0; j <= 255; j++)
+            for (int j = 0; j < width; j++)
             {
-                image << i << ' ' << j << ' ' << 100 << '\n';
+                double red = (double(i) / double(height)) * 255;
+                double green = (double(j) / double(width)) * 255;
+                image << int(red) << ' ' << int(green) << ' ' << 100 << '\n';
+                cout << "Pixels remaining: " << remaining << '\n';
+                remaining--;
             }
         }
         image.close();
