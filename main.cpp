21b7ac9b3d12d509a5114688fa3abdd254b0edcc
shahmeerathar <shahmeer.athar@gmail.com>
2020-08-21 21:51:26 -0400

    Added .ppm image file output function

--- main.cpp
+++ main.cpp
@@ -1,7 +1,28 @@
-# include <iostream>
+#include <iostream>
+#include <fstream>
 using namespace std;
+
+void write_ppm()
+{
+    ofstream image ("image.ppm");
+    if (image.is_open())
+    {
+        int width = 256;
+        int height = 256;
+        image << "P3\n" << width << ' ' << height << "\n255\n";
+        for (int i = 0; i <= 255; i++)
+        {
+            for (int j = 0; j <= 255; j++)
+            {
+                image << i << ' ' << j << ' ' << 100 << '\n';
+            }
+        }
+        image.close();
+    }
+}
 
 int main()
 {
+    write_ppm();
     return 0;
 }
