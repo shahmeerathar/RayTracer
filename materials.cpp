0730c67287c78a74ae624ccca77874c2aafba4c1
shahmeerathar <shahmeer.athar@gmail.com>
2020-08-27 07:10:09 -0400

    Implmented fuzz for metals

--- materials.cpp
+++ materials.cpp
@@ -11,7 +11,7 @@ bool Lambertian::scatter(const ray& in_ray, const hit_record& record, colour& at
 bool metal::scatter(const ray& in_ray, const hit_record& record, colour& attenuation, ray& scattered) const
 {
     vec3 reflected_direction = reflect(unit(in_ray.direction), record.normal);
-    scattered = ray(record.point, reflected_direction);
+    scattered = ray(record.point, reflected_direction + random_double(0.3, 1.0) * random_unit_vector());
     attenuation = albedo;
     return (dot(scattered.direction, record.normal) > 0);
 }
