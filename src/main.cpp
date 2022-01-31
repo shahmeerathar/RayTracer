923fa7c3ae28d601a2a7f831bb784fe78bfc739b
Shahmeer Athar <shahmeer.athar@gmail.com>
2022-01-31 15:57:34 -0500

    Refactor for consistent style: main

--- src/main.cpp
+++ src/main.cpp
@@ -15,78 +15,80 @@
 #define NS_PRIVATE_IMPLEMENTATION
 #define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
+
 #include <Metal/Metal.hpp>
 
 using namespace std;
 
-
-Colour colour_ray(const Ray& r, const Hittable& object, int depth)
+Colour colourRay(const Ray &r, const Hittable &object, int depth)
 {
-	HitRecord record;
+    HitRecord record;
 
     if (depth > 50)
     {
         return {0.0, 0.0, 0.0};
     }
 
-	if (object.hit(r, 0.001, infinity, record))
-	{
+    if (object.hit(r, 0.001, infinity, record))
+    {
         Ray scattered = r;
         Colour attenuation;
+
         if (record.mtrPtr->scatter(r, record, attenuation, scattered))
         {
-            return attenuation * colour_ray(scattered, object, depth + 1);
+            return attenuation * colourRay(scattered, object, depth + 1);
         }
-        return Colour(0.0, 0.0, 0.0);
-	}
 
-    Vec3 unit_direction = unit(r.direction);
-    auto t = 0.5*(unit_direction.y + 1.0);
-    return (1.0-t) * Colour(1.0, 1.0, 1.0) + t * Colour(0.5, 0.7, 1.0);
+        return {0.0, 0.0, 0.0};
+    }
+
+    Vec3 unitDirection = unit(r.direction);
+    auto t = 0.5 * (unitDirection.y + 1.0);
+    return (1.0 - t) * Colour(1.0, 1.0, 1.0) + t * Colour(0.5, 0.7, 1.0);
 }
 
-Camera get_camera(double aspect_ratio)
+Camera getCamera(double aspect_ratio)
 {
     Point3 lookfrom = Point3(3, 3, 2);
     Point3 lookat = Point3(0, 0, -1);
     Point3 vup = Vec3(0, 1, 0);
-    double fov_angle = 20.0;
+    double fovAngle = 20.0;
     double aperture = 2.0;
-    double focus_distance = (lookfrom - lookat).length();
+    double focusDistance = (lookfrom - lookat).length();
     bool dof = false;
-    Camera cam(lookfrom, lookat, vup, fov_angle, aspect_ratio, aperture, focus_distance, dof);
+    Camera cam(lookfrom, lookat, vup, fovAngle, aspect_ratio, aperture, focusDistance, dof);
 
     return cam;
 }
 
-HittableList get_scene()
+HittableList getScene()
 {
     //Defining objects and materials
-    shared_ptr<Material> material_1 = make_shared<Lambertian>(Colour(0.8, 0.8, 0.0));
-    shared_ptr<Material> material_2 = make_shared<Lambertian>(Colour(0.7, 0.3, 0.3));
-    shared_ptr<Material> material_3 = make_shared<Metal>(Colour(0.8, 0.8, 0.8), 0.001);
-    shared_ptr<Material> material_4 = make_shared<Metal>(Colour(0.8, 0.6, 0.2), 0.2);
-    shared_ptr<Material> material_5 = make_shared<Dielectric>(1.5);
+    shared_ptr<Material> material1 = make_shared<Lambertian>(Colour(0.8, 0.8, 0.0));
+    shared_ptr<Material> material2 = make_shared<Lambertian>(Colour(0.7, 0.3, 0.3));
+    shared_ptr<Material> material3 = make_shared<Metal>(Colour(0.8, 0.8, 0.8), 0.001);
+    shared_ptr<Material> material4 = make_shared<Metal>(Colour(0.8, 0.6, 0.2), 0.2);
+    shared_ptr<Material> material5 = make_shared<Dielectric>(1.5);
 
     HittableList objects = HittableList();
-    objects.add(make_shared<Sphere>(Point3(0.0, 0.0, -1), 0.5, material_1));
-    objects.add(make_shared<Sphere>(Point3(0.0, -100.5, -1), 100, material_2));
-    objects.add(make_shared<Sphere>(Point3(0.25, 0.25, -0.5), 0.1, material_4));
-    objects.add(make_shared<Sphere>(Point3(-0.25, 0.25, -0.5), 0.1, material_3));
-    objects.add(make_shared<Sphere>(Point3(0.0, 0.3, -0.6), 0.15, material_5));
+    objects.add(make_shared<Sphere>(Point3(0.0, 0.0, -1), 0.5, material1));
+    objects.add(make_shared<Sphere>(Point3(0.0, -100.5, -1), 100, material2));
+    objects.add(make_shared<Sphere>(Point3(0.25, 0.25, -0.5), 0.1, material4));
+    objects.add(make_shared<Sphere>(Point3(-0.25, 0.25, -0.5), 0.1, material3));
+    objects.add(make_shared<Sphere>(Point3(0.0, 0.3, -0.6), 0.15, material5));
 
     HittableList world;
 
-    auto material_ground = make_shared<Lambertian>(Colour(0.8, 0.8, 0.0));
-    auto material_center = make_shared<Lambertian>(Colour(0.1, 0.2, 0.5));
-    auto material_left = make_shared<Lambertian>(Colour(0.5, 0.2, 0.3));
-    auto material_right  = make_shared<Metal>(Colour(0.8, 0.6, 0.2), 0.0);
+    auto materialGround = make_shared<Lambertian>(Colour(0.8, 0.8, 0.0));
+    auto materialCenter = make_shared<Lambertian>(Colour(0.1, 0.2, 0.5));
+    auto materialLeft = make_shared<Lambertian>(Colour(0.5, 0.2, 0.3));
+    auto materialRight = make_shared<Metal>(Colour(0.8, 0.6, 0.2), 0.0);
 
-    world.add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, material_center));
-    world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, material_left));
-    world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), -0.45, material_left));
-    world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, material_right));
+    world.add(make_shared<Sphere>(Point3(0.0, -100.5, -1.0), 100.0, materialGround));
+    world.add(make_shared<Sphere>(Point3(0.0, 0.0, -1.0), 0.5, materialCenter));
+    world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), 0.5, materialLeft));
+    world.add(make_shared<Sphere>(Point3(-1.0, 0.0, -1.0), -0.45, materialLeft));
+    world.add(make_shared<Sphere>(Point3(1.0, 0.0, -1.0), 0.5, materialRight));
 
     return world;
 }
@@ -94,63 +96,64 @@ HittableList get_scene()
 int main()
 {
     //Defining image properties and Camera
-    double aspect_ratio = 1.0/1.0;
-    int img_width  = 1200;
-    int img_height = static_cast<int>(static_cast<double>(img_width) / aspect_ratio);
-    int samples_per_pixel = 100;
+    double aspectRatio = 1.0 / 1.0;
+    int imgWidth = 1200;
+    int imgHeight = static_cast<int>(static_cast<double>(imgWidth) / aspectRatio);
+    int samplesPerPixel = 100;
 
-    Camera cam = get_camera(aspect_ratio);
-    HittableList scene = get_scene();
+    Camera cam = getCamera(aspectRatio);
+    HittableList scene = getScene();
 
-    MTL::Device* device = MTL::CreateSystemDefaultDevice();
+    MTL::Device *device = MTL::CreateSystemDefaultDevice();
     cout << device->name()->utf8String();
 
-    int no_of_threads = std::thread::hardware_concurrency();
+    int noOfThreads = std::thread::hardware_concurrency();
 
-    ofstream image ("../renders/image.ppm");
+    ofstream image("../renders/image.ppm");
     if (image.is_open())
     {
         int index = 0;
-        image << "P3\n" << img_width << ' ' << img_height << "\n255\n";
-        for (int i = img_height; i > 0; i--)
+        image << "P3\n" << imgWidth << ' ' << imgHeight << "\n255\n";
+        for (int i = imgHeight; i > 0; i--)
         {
-            for (int j = 0; j < img_width; j = j + no_of_threads)
+            for (int j = 0; j < imgWidth; j = j + noOfThreads)
             {
                 mutex mutex;
-                auto render_pixel = [&] (int x)
+                auto renderPixel = [&](int x)
                 {
                     Colour pixel = Colour(0.0, 0.0, 0.0);
-                    for (int s = 0; s < samples_per_pixel; s++)
+                    for (int s = 0; s < samplesPerPixel; s++)
                     {
-                        double u = (static_cast<double>(x) + randomDouble()) / static_cast<double>(img_width);
-                        double v = (static_cast<double>(i) + randomDouble()) / static_cast<double>(img_height);
+                        double u = (static_cast<double>(x) + randomDouble()) / static_cast<double>(imgWidth);
+                        double v = (static_cast<double>(i) + randomDouble()) / static_cast<double>(imgHeight);
                         Ray r = cam.getRay(u, v);
-                        pixel += colour_ray(r, scene, 1);
+                        pixel += colourRay(r, scene, 1);
                     }
-                    pixel *= (1.0 / static_cast<double>(samples_per_pixel));
+                    pixel *= (1.0 / static_cast<double>(samplesPerPixel));
                     return pixel;
                 };
-                auto output_pixel = [&] (Colour pixel)
+                auto outputPixel = [&](Colour pixel)
                 {
                     int red = static_cast<int>(pixel.x * 255);
                     int green = static_cast<int>(pixel.y * 255);
                     int blue = static_cast<int>(pixel.z * 255);
                     mutex.lock();
                     image << red << ' ' << green << ' ' << blue << '\n';
-                    cout << "Rendered: " << (index*100)/(img_width*img_height) << '%' << " - remaining pixels: " << img_width*img_height - 1 - index << '\n';
+                    cout << "Rendered: " << (index * 100) / (imgWidth * imgHeight) << '%' << " - remaining pixels: "
+                         << imgWidth * imgHeight - 1 - index << '\n';
                     mutex.unlock();
                     index++;
                 };
 
                 //Concurrency
                 vector<std::future<Colour>> futures;
-                for(int i = 0; i < no_of_threads; i++)
+                for (int idx = 0; idx < noOfThreads; idx++)
                 {
-                    futures.push_back(std::async(render_pixel, j + i));
+                    futures.push_back(std::async(renderPixel, j + idx));
                 }
-                for (auto& f : futures)
+                for (auto &f: futures)
                 {
-                    output_pixel(f.get());
+                    outputPixel(f.get());
                 }
             }
         }
