5f2fe466b748f7fcdbec9763083bfea79b679431
Shahmeer Athar <shahmeer.athar@gmail.com>
2022-01-31 22:11:15 -0500

    Metal setup

--- src/GPURenderer.cpp
+++ src/GPURenderer.cpp
@@ -5,16 +5,33 @@
 #include "GPURenderer.h"
 
 #define NS_PRIVATE_IMPLEMENTATION
-#define CA_PRIVATE_IMPLEMENTATION
 #define MTL_PRIVATE_IMPLEMENTATION
 #include <Foundation/Foundation.hpp>
 #include <Metal/Metal.hpp>
-#include <QuartzCore/QuartzCore.hpp>
 
 void GPURenderer::render()
 {
-    MTL::Device *device = MTL::CreateSystemDefaultDevice();
-    auto gpuName = device->name()->utf8String();
-
+    // Setting up Metal
+    MTL::Device *mtlDevice = MTL::CreateSystemDefaultDevice();
+    auto gpuName = mtlDevice->name()->utf8String();
     cout << "Rendering on " << gpuName << endl;
+
+    MTL::CommandQueue *commandQueue = mtlDevice->newCommandQueue();
+    MTL::Library *defaultLibrary = mtlDevice->newDefaultLibrary();
+    MTL::Function *rayTracing = defaultLibrary->newFunction(reinterpret_cast<const NS::String *>("rayTracing"));
+    NS::Error **nsError;
+    MTL::ComputePipelineState *rtPipelineState = mtlDevice->newComputePipelineState(rayTracing, nsError);
+
+    int threadsPerGroupWidth = rtPipelineState->threadExecutionWidth();
+    int threadsPerGroupHeight = rtPipelineState->maxTotalThreadsPerThreadgroup() / threadsPerGroupWidth;
+    MTL::Size threadsPerGroup = MTL::Size(threadsPerGroupWidth, threadsPerGroupHeight, 1);
+    MTL::Size threadsPerGrid = MTL::Size(m_ImageProperties.imgWidth, m_ImageProperties.imgHeight, 1);
+    MTL::CommandBuffer *commandBuffer = commandQueue->commandBuffer();
+    MTL::ComputeCommandEncoder *encoder = commandBuffer->computeCommandEncoder();
+    encoder->setComputePipelineState(rtPipelineState);
+
+    encoder->dispatchThreads(threadsPerGrid, threadsPerGroup);
+    encoder->endEncoding();
+    commandBuffer->commit();
+    commandBuffer->waitUntilCompleted();
 }
