85c91f6b689e0312300aa954578a7ee230756c3b
Shahmeer Athar <shahmeer.athar@gmail.com>
2022-02-02 03:35:47 -0500

    Fixed texture data buffer overflow and defined struct for pixel properties

--- src/GPURayTracer.metal
+++ src/GPURayTracer.metal
@@ -5,5 +5,5 @@ using namespace metal;
 kernel void rayTrace(texture2d<half, access::write>  renderTexture  [[texture(0)]],
                      uint2 index [[thread_position_in_grid]])
 {
-    renderTexture.write(half4(1.0 * index[0], 0.5, 1.0 * index[1], 1.0), index);
+    renderTexture.write(half4(1.0 * index[0] / 1200, 0.3, 0.5 * index[1] / 1200, 1.0), index);
 }
--- src/GPURenderer.cpp
+++ src/GPURenderer.cpp
@@ -13,6 +13,14 @@
 #include <Foundation/Foundation.hpp>
 #include <Metal/Metal.hpp>
 
+struct PixelProperties
+{
+    MTL::PixelFormat pixelFormat = MTL::PixelFormatRGBA32Float;
+    int pixelComponentSize = 4;
+    int componentsPerPixel = 4;
+    int bytesPerPixel = pixelComponentSize * componentsPerPixel;
+} typedef PixelProperties;
+
 void GPURenderer::render()
 {
     // Setting up Metal device and command queue
@@ -60,8 +68,9 @@ void GPURenderer::render()
     encoder->setComputePipelineState(rtPipelineState);
 
     // Set up texture for our ray tracing shader
+    PixelProperties pixelProperties;
     MTL::TextureDescriptor *textureDescriptor = MTL::TextureDescriptor::alloc()->init();
-    textureDescriptor->setPixelFormat(MTL::PixelFormatRGBA32Float);
+    textureDescriptor->setPixelFormat(pixelProperties.pixelFormat);
     textureDescriptor->setWidth(m_ImageProperties.imgWidth);
     textureDescriptor->setHeight(m_ImageProperties.imgHeight);
     textureDescriptor->setUsage(MTL::TextureUsageShaderWrite);
@@ -77,12 +86,11 @@ void GPURenderer::render()
 
     // Output to image
     cout << "Shader has completed render pass! Outputting image now" << endl;
-    int stride = m_ImageProperties.imgWidth * 4 * 4;
-    int numBytes = stride * m_ImageProperties.imgHeight;
+    int bytesPerRow = m_ImageProperties.imgWidth * pixelProperties.bytesPerPixel;
+    int numBytes = bytesPerRow * m_ImageProperties.imgHeight;
     auto textureData = (float *)malloc(numBytes);
     MTL::Region mtlRegion = MTL::Region::Make2D(0, 0, m_ImageProperties.imgWidth, m_ImageProperties.imgHeight);
-    texture->getBytes(textureData, stride, numBytes, mtlRegion, 0, 0);
-    cout << textureData << endl;
+    texture->getBytes(textureData, bytesPerRow, numBytes, mtlRegion, 0, 0);
 
     ofstream image("../renders/image.ppm");
     if (!image.is_open())
@@ -91,20 +99,18 @@ void GPURenderer::render()
         return;
     }
     image << "P3\n" << m_ImageProperties.imgWidth << ' ' << m_ImageProperties.imgHeight << "\n255\n";
-    for (int i = 0; i < numBytes; i+=4)
+    for (int i = 0; i < numBytes / pixelProperties.componentsPerPixel; i+=4)
     {
-        cout << i << endl;
         int red = static_cast<int>(textureData[i] * 255);
         int green = static_cast<int>(textureData[i + 1] * 255);
         int blue = static_cast<int>(textureData[i + 2] * 255);
         int alpha = static_cast<int>(textureData[i + 3] * 255);
 
-        // cout << red << ' ' << green << ' ' << blue << ' '  << alpha << endl;
         image << red << ' ' << green << ' ' << blue << endl;
     }
     free(textureData);
 
-    // Release objects
+    // Release Cocoa objects
     metalLibFilePath->release();
     kernelFunctionName->release();
     mtlDevice->release();
